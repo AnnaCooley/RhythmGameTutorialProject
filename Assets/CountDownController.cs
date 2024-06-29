10058537638b2b894cf6d31d3453aace79ed57a9
AnnaCooley <132299398+AnnaCooley@users.noreply.github.com>
2024-06-28 23:14:49 -0700

    Count Down Timer
    - Attempted adding countdown timer
    - No object reference set for the following objects after 3
    - Having trouble calling SongManager.StartSong();

---
+++ Assets/CountDownController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownController : MonoBehaviour
+{
+    public int countdownTime;
+    public Text countdownDisplay;
+
+    void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while(countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countdownTime--;
+        }
+
+        countdownDisplay.text = "GO!";
+
+        yield return new WaitForSeconds(1f);
+
+        countdownDisplay.gameObject.SetActive(false);
+
+    }
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -45,54 +45,55 @@ public class SongManager : MonoBehaviour
         }
     }
 
-    private IEnumerator ReadFromWebsite()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get(Application.streamingAssetsPath + "/" + fileLocation))
+        private IEnumerator ReadFromWebsite()
         {
-            yield return www.SendWebRequest();
+            using (UnityWebRequest www = UnityWebRequest.Get(Application.streamingAssetsPath + "/" + fileLocation))
+            {
+                yield return www.SendWebRequest();
 
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.LogError(www.error);
-            }
-            else
-            {
-                byte[] results = www.downloadHandler.data;
-                using (var stream = new MemoryStream(results))
+                if (www.isNetworkError || www.isHttpError)
                 {
-                    midiFile = MidiFile.Read(stream);
-                    GetDataFromMidi();
+                    Debug.LogError(www.error);
+                }
+                else
+                {
+                    byte[] results = www.downloadHandler.data;
+                    using (var stream = new MemoryStream(results))
+                    {
+                        midiFile = MidiFile.Read(stream);
+                        GetDataFromMidi();
+                    }
                 }
             }
         }
-    }
 
-    private void ReadFromFile()
-    {
-        midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
-        GetDataFromMidi();
-    }
-    public void GetDataFromMidi()
-    {
-        var notes = midiFile.GetNotes();
-        var array = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
-        notes.CopyTo(array, 0);
+        private void ReadFromFile()
+        {
+            midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
+            GetDataFromMidi();
+        }
+        public void GetDataFromMidi()
+        {
+            var notes = midiFile.GetNotes();
+            var array = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
+            notes.CopyTo(array, 0);
 
-        foreach (var lane in lanes) lane.SetTimeStamps(array);
+            foreach (var lane in lanes) lane.SetTimeStamps(array);
 
-        Invoke(nameof(StartSong), songDelayInSeconds);
-    }
-    public void StartSong()
-    {
-        audioSource.Play();
-    }
-    public static double GetAudioSourceTime()
-    {
-        return (double)Instance.audioSource.timeSamples / Instance.audioSource.clip.frequency;
-    }
+          Invoke(nameof(StartSong), songDelayInSeconds);
+        }
+        public void StartSong()
+        {
+            audioSource.Play();
+        }
+        public static double GetAudioSourceTime()
+        {
+            return (double)Instance.audioSource.timeSamples / Instance.audioSource.clip.frequency;
+        }
 
-    void Update()
-    {
-        
-    }
+        void Update()
+        {
+
+        }
+
 }
